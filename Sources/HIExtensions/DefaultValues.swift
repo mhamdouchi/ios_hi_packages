86ffe40fd4769852aaeaf3aeefe2a4d7bdc50f1c
Mohamed Hamdouchi <career@hamdouchi.com>
2022-02-08 03:35:17 -0500

    WIP

---
+++ Sources/HIExtensions/DefaultValues.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultValues.swift
+//  
+//
+//  Created by Mohamed Hamdouchi on 2/8/22.
+//
+
+import Foundation
+
+public let kDotString: String = "."
+public let kEmptyString: String = ""
+public let kEmptyStringSubSequence: String.SubSequence = ""
+public let kSpaceString: String = " "
