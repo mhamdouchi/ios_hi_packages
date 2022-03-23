25d07a660988c492c3164723a4aae7dfd99b932a
mhamdouchi <career@hamdouchi.com>
2022-03-23 12:46:16 -0400

    Adds language to localized string

--- Sources/HIExtensions/Extensions/StringExtension.swift
+++ Sources/HIExtensions/Extensions/StringExtension.swift
@@ -40,8 +40,19 @@ public extension String {
 
 // MARK: - Localizable
 public extension String {
-    func localized() -> String {
-        NSLocalizedString(self, comment: kEmptyString)
+    func localized(_ language: String? = nil) -> String {
+        let localizedString: String
+
+        if let language = language,
+           let path = Bundle.main.path(forResource: language, ofType: "lproj"),
+           let bundle = Bundle(path: path)
+        {
+            localizedString = NSLocalizedString(self, tableName: nil, bundle: bundle, value: kEmptyString, comment: kEmptyString)
+        } else {
+            localizedString = NSLocalizedString(self, comment: kEmptyString)
+        }
+
+        return localizedString
     }
 
     func localized(with arguments: [CVarArg]) -> String {
