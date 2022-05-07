538a2a7443b389382fb8cf7033d9fb5d3ad8a93d
mhamdouchi <career@hamdouchi.com>
2022-05-06 21:57:02 -0400

    Adds Constraint extension

--- Sources/HIExtensions/Extensions/UIViewExtension.swift
+++ Sources/HIExtensions/Extensions/UIViewExtension.swift
@@ -8,10 +8,8 @@
 import UIKit
 
 public extension UIView {
-    typealias EmptyCompletion = () -> Void
-
     var globalRect: CGRect? {
-        return superview?.convert(frame, to: nil)
+        superview?.convert(frame, to: nil)
     }
 
     func addParallaxEffect() {
@@ -104,6 +102,8 @@ public extension UIView {
         })
     }
 
+    typealias EmptyCompletion = () -> Void
+
     func bounce(withScale scale: CGFloat = 1.2, andDuration duration: Double = 0.25, andDelay delay: TimeInterval = .zero, andDamping damping: CGFloat = 0.1, andVelocity velocity: CGFloat = 10.0, _ completion: EmptyCompletion? = nil) {
         transform = CGAffineTransform(scaleX: scale, y: scale)
         UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: damping, initialSpringVelocity: velocity, options: .allowUserInteraction) {
@@ -169,5 +169,297 @@ public extension UIView {
         if let borderColor = borderColor {
             layer.borderColor = borderColor.cgColor
         }
+    }
+}
+
+// MARK: - Frame
+extension UIView {
+    var x: CGFloat {
+        get { frame.origin.x }
+        set { frame = CGRect(x: newValue, y: frame.origin.y, width: frame.size.width, height: frame.size.height) }
+    }
+
+    var top: CGFloat {
+        get { frame.origin.y }
+        set { frame = CGRect(x: frame.origin.x, y: newValue, width: frame.size.width, height: frame.size.height) }
+    }
+
+    var width: CGFloat {
+        get { frame.size.width }
+        set { frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: newValue, height: frame.size.height) }
+    }
+
+    var height: CGFloat {
+        get { frame.size.height }
+        set { frame = CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: newValue) }
+    }
+
+    var y: CGFloat {
+        get { x + width }
+        set { x = newValue - width }
+    }
+
+    var bottom: CGFloat {
+        get { top + height }
+        set { top = newValue - height }
+    }
+
+    var centerX: CGFloat {
+        get { x + width * 0.5 }
+        set { x = newValue - width * 0.5 }
+    }
+
+    var centerY: CGFloat {
+        get { top + height * 0.5 }
+        set { top = newValue - height * 0.5 }
+    }
+
+    var relativeCenter: CGPoint {
+        CGPoint(x: bounds.width / 2.0, y: bounds.height / 2.0)
+    }
+
+    var size: CGSize {
+        get { frame.size }
+        set { frame.size = newValue }
+    }
+}
+
+// MARK: - Constraint
+public extension UIView {
+    @discardableResult
+    func align(with view: UIView, constant: CGFloat = .zero) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant),
+            rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant),
+            topAnchor.constraint(equalTo: view.topAnchor, constant: constant),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        return constraints
+    }
+
+    @discardableResult
+    func alignLeadingTrailing(with view: UIView, constant: CGFloat = .zero) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            leftAnchor.constraint(equalTo: view.leftAnchor, constant: constant),
+            rightAnchor.constraint(equalTo: view.rightAnchor, constant: -constant),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        return constraints
+    }
+
+    @discardableResult
+    func alignTopBottom(with view: UIView, constant: CGFloat = .zero) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            topAnchor.constraint(equalTo: view.topAnchor, constant: constant),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -constant),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        return constraints
+    }
+
+    @discardableResult
+    func alignTopBottom(with layoutGuide: UILayoutGuide, constant: CGFloat = .zero) -> [NSLayoutConstraint] {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            topAnchor.constraint(equalTo: layoutGuide.topAnchor, constant: constant),
+            bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor, constant: -constant),
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        return constraints
+    }
+
+    @discardableResult
+    func alignTop(to anchor: NSLayoutYAxisAnchor, constant: CGFloat = .zero,
+                  priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint
+    {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = topAnchor.constraint(equalTo: anchor, constant: constant)
+
+        constraint.priority = priority
+        constraint.isActive = isActive
+
+        return constraint
+    }
+
+    @discardableResult
+    func alignTop(with guide: UILayoutGuide, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignTop(to: guide.topAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignTop(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignTop(to: view.topAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignBottom(to anchor: NSLayoutYAxisAnchor, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = bottomAnchor.constraint(equalTo: anchor, constant: constant)
+
+        constraint.priority = priority
+        constraint.isActive = isActive
+
+        return constraint
+    }
+
+    @discardableResult
+    func alignBottom(with guide: UILayoutGuide, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignBottom(to: guide.bottomAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignBottom(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignBottom(to: view.bottomAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignLeading(to anchor: NSLayoutXAxisAnchor, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = leadingAnchor.constraint(equalTo: anchor, constant: constant)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func alignLeading(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignLeading(to: view.leadingAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignTrailing(to anchor: NSLayoutXAxisAnchor, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = trailingAnchor.constraint(equalTo: anchor, constant: constant)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func alignTrailing(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignTrailing(to: view.trailingAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignBottom(toTopOf view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignBottom(to: view.topAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func alignTrailing(toLeadingOf view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        alignTrailing(to: view.leadingAnchor, constant: constant, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func centerVertical(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func centerHorizontal(with view: UIView, constant: CGFloat = .zero, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        let constraint = centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: constant)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func constrainHeight(_ value: CGFloat, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = heightAnchor.constraint(equalToConstant: value)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func constrainWidth(_ value: CGFloat, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = widthAnchor.constraint(equalToConstant: value)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func constrainWidth(to view: UIView, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = widthAnchor.constraint(equalTo: view.widthAnchor)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func constrainHeight(to view: UIView, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = heightAnchor.constraint(equalTo: view.heightAnchor)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    func constraintSize(width: CGFloat, height: CGFloat, priority: UILayoutPriority = .required, isActive: Bool = true) {
+        constrainWidth(width, priority: priority, isActive: isActive)
+        constrainHeight(height, priority: priority, isActive: isActive)
+    }
+
+    @discardableResult
+    func constrainHeightToWidth(ratio: CGFloat = 1, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = heightAnchor.constraint(equalTo: widthAnchor, multiplier: ratio)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
+    }
+
+    @discardableResult
+    func constrainWidthToHeight(ratio: CGFloat = 1, priority: UILayoutPriority = .required, isActive: Bool = true) -> NSLayoutConstraint {
+        let constraint = widthAnchor.constraint(equalTo: heightAnchor, multiplier: ratio)
+
+        constraint.isActive = isActive
+        constraint.priority = priority
+
+        return constraint
     }
 }
