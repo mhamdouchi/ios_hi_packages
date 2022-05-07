1fb8918a0a5c5324086c071a5d914c8847fd1e36
mhamdouchi <career@hamdouchi.com>
2022-05-06 20:58:28 -0400

    Adds PaddingButton class

---
+++ Sources/HIExtensions/Classes/PaddingButton.swift
@@ -0,0 +1,30 @@
+//
+//  PaddingButton.swift
+//
+//
+//  Created by Mohamed Hamdouchi on 5/6/22.
+//
+
+import UIKit
+
+public class PaddingButton: UIButton {
+    private let widthOffset: CGFloat
+    private let heightOffset: CGFloat
+
+    public init(frame: CGRect, widthOffset: CGFloat = 20, heightOffset: CGFloat = 10) {
+        self.widthOffset = widthOffset
+        self.heightOffset = heightOffset
+
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override public var intrinsicContentSize: CGSize {
+        let baseSize = super.intrinsicContentSize
+        return CGSize(width: baseSize.width + 20, height: baseSize.height)
+    }
+}
