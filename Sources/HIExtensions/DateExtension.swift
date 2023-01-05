f50eaed4344ecc974766877a1b100d8d658c57f5
mhamdouchi <career@hamdouchi.com>
2023-01-05 08:35:09 -0500

    Adds a static property `now`

--- Sources/HIExtensions/DateExtension.swift
+++ Sources/HIExtensions/DateExtension.swift
@@ -49,6 +49,12 @@ public extension Date {
 }
 
 public extension Date {
+    static func now() -> String {
+        Date().format(.iso8601WithFraction)
+    }
+}
+
+public extension Date {
     enum DateFormats {
         case full
         case iso8601
