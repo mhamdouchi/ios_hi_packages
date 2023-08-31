a2afca67104e8c0928e6d38440b9e2bd169c6a87
mhamdouchi <career@hamdouchi.com>
2023-08-31 17:52:40 -0400

    Adds UIButton Extension

---
+++ Sources/HIExtensions/UIButtonExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UIButtonExtension.swift
+//  
+//
+//  Created by Mohamed Hamdouchi on 8/31/23.
+//
+
+import UIKit
+
+public extension UIButton {
+    func bringTitleAndImageToFront() {
+        if let titleLabel = titleLabel, let imageView = imageView {
+            bringSubviewToFront(titleLabel)
+            bringSubviewToFront(imageView)
+        }
+    }
+}
