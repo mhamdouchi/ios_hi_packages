d5735313924d9ed63fd51d0b25068fd3f3fc458f
mhamdouchi <career@hamdouchi.com>
2023-09-10 13:34:05 -0400

    Adds localizedPrice for SKProduct

---
+++ Sources/HIExtensions/SKProductExtension.swift
@@ -0,0 +1,21 @@
+//
+//  SKProductExtension.swift
+//  
+//
+//  Created by Mohamed Hamdouchi on 9/10/23.
+//
+
+import Foundation
+import StoreKit
+
+extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+
+        formatter.formatterBehavior = .behavior10_4
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+
+        return formatter.string(from: price) ?? price.description
+    }
+}
