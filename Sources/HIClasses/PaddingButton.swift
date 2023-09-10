482d8001ee4caed0cb3d6c028add6b83aeb47c37
mhamdouchi <career@hamdouchi.com>
2023-09-10 14:21:36 -0400

    Makes PaddingButton open instead of public to allow inheritance

--- Sources/HIClasses/PaddingButton.swift
+++ Sources/HIClasses/PaddingButton.swift
@@ -7,20 +7,13 @@
 
 import UIKit
 
-public class PaddingButton: UIButton {
-    private let widthOffset: CGFloat
-    private let heightOffset: CGFloat
-
-    public init(frame: CGRect, widthOffset: CGFloat = 20, heightOffset: CGFloat = 10) {
-        self.widthOffset = widthOffset
-        self.heightOffset = heightOffset
-
-        super.init(frame: frame)
-    }
-
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+@IBDesignable
+open class PaddingButton: UIButton {
+    @IBInspectable var widthOffset: CGFloat = 20
+    @IBInspectable var heightOffset: CGFloat = 10
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 
     override public var intrinsicContentSize: CGSize {
