f0d92c5096297b7201acf0bc4b8509ed1fe9613e
mhamdouchi <career@hamdouchi.com>
2023-09-25 13:39:27 -0400

    Adds environment var to Bundle extension

--- Sources/HIExtensions/BundleExtension.swift
+++ Sources/HIExtensions/BundleExtension.swift
@@ -8,11 +8,19 @@
 import Foundation
 
 public extension Bundle {
-    var versionNumber: String? {
-        infoDictionary?["CFBundleShortVersionString"] as? String
-    }
-
     var buildNumber: String? {
         infoDictionary?["CFBundleVersion"] as? String
+    }
+
+    var environment: String {
+        #if DEBUG
+            return "dev"
+        #else
+            return "prod"
+        #endif
+    }
+
+    var versionNumber: String? {
+        infoDictionary?["CFBundleShortVersionString"] as? String
     }
 }
