0d07478f4c0fbd2d9da7e7212f2e7cd3c5141427
mhamdouchi <career@hamdouchi.com>
2023-11-15 11:23:25 -0500

    Adds drop shadow for SwiftUI

--- Sources/HIExtensions/StringExtensions/String+UUID.swift
+++ Sources/HIExtensions/StringExtensions/String+UUID.swift
@@ -12,12 +12,12 @@ public extension String {
     static func generateUUID() -> String {
         UUID().uuidString.uppercased()
     }
-    
+
     func isValidUUIDString() -> Bool {
         let uuidPattern = #"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"#
         let regex = try! NSRegularExpression(pattern: uuidPattern, options: [])
         let range = NSRange(location: 0, length: utf16.count)
-        
+
         return regex.firstMatch(in: self, options: [], range: range) != nil
     }
 }
