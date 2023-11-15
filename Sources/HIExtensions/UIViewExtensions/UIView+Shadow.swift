28f21e9bebe7658e40a1bcbc8e7c991c3770e0cf
mhamdouchi <career@hamdouchi.com>
2023-11-15 11:23:25 -0500

    Adds drop shadow for SwiftUI

--- Sources/HIExtensions/UIViewExtensions/UIView+Shadow.swift
+++ Sources/HIExtensions/UIViewExtensions/UIView+Shadow.swift
@@ -5,35 +5,47 @@
 //  Created by Mohamed Hamdouchi on 2/8/22.
 //
 
+import SwiftUI
 import UIKit
 
 // MARK: - Shadow
-public extension UIView {
-    enum ShadowStyle {
-        case subtle
-        case light
-        case medium
-        case heavy
+public enum ShadowStyle {
+    case subtle
+    case light
+    case medium
+    case heavy
 
-        var radius: CGFloat {
-            switch self {
-                case .subtle:
-                    return 1.0
-                case .light:
-                    return 2.0
-                case .medium:
-                    return 5.0
-                case .heavy:
-                    return 8.0
-            }
+    var radius: CGFloat {
+        switch self {
+            case .subtle:
+                return 1.0
+            case .light:
+                return 2.0
+            case .medium:
+                return 5.0
+            case .heavy:
+                return 8.0
         }
     }
+}
 
+public extension UIView {
     func dropShadow(_ style: ShadowStyle = .medium, color: UIColor = .black) {
         layer.masksToBounds = false
         layer.shadowColor = color.cgColor
         layer.shadowOpacity = 0.2
         layer.shadowOffset = CGSize.zero
         layer.shadowRadius = style.radius
+    }
+}
+
+public extension View {
+    func dropShadow(_ style: ShadowStyle = .medium, color: Color = .black) -> some View {
+        shadow(
+            color: color,
+            radius: style.radius,
+            x: 0,
+            y: 0
+        )
     }
 }
